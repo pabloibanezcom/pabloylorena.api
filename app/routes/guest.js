aefd45026a03e108ff2f1d7234953bc38c1cc62e
Pablo Ibanez <me@pabloibanez.com>
2018-04-11 12:11:35 +0100

    Table management added

--- app/routes/guest.js
+++ app/routes/guest.js
@@ -13,6 +13,18 @@ module.exports = (app, modelsService) => {
         app.routesInfo['Guest'].push({ model: 'Guest', name: 'Update guest order',  method: 'PUT', url: url, body: { order: 0 } });
     }
 
+    const registerUpdateGuestOrderInTable = () => {
+        const url = '/api/guest/order-table/:id';
+        app.put(url,
+            (req, res) => {
+                service.updateGuestOrderInTable(modelsService, req.params.id, req.body.orderInTable)
+                    .then(result => res.status(result.statusCode).send(result.data))
+                    .catch(err => res.status(500).send(err) );
+            });
+        app.routesInfo['Guest'].push({ model: 'Guest', name: 'Update guest order in table',  method: 'PUT', url: url, body: { orderInTable: 0 } });
+    }
+
     registerUpdateGuestOrder();
+    registerUpdateGuestOrderInTable();
     
 };
--- app/services/guest.service.js
+++ app/services/guest.service.js
@@ -8,4 +8,11 @@ service.updateGuestOrder = async (modelsService, guestId, order) => {
     return { statusCode: 200, data: guest };
 }
 
+service.updateGuestOrderInTable = async (modelsService, guestId, orderInTable) => {
+    const guest = await modelsService.getModel('Guest').findOne({ _id: guestId });
+    guest.orderInTable = orderInTable;
+    await guest.save()
+    return { statusCode: 200, data: guest };
+}
+
 module.exports = service;
