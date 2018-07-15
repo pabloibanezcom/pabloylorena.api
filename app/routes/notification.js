88bc5989b22772f16950d618d9102f7be87f1727
Pablo Ibanez <me@pabloibanez.com>
2018-07-15 14:28:26 +0100

    Notification changes

--- app/routes/notification.js
+++ app/routes/notification.js
@@ -2,6 +2,17 @@ const service = require('../services/notification.service');
 
 module.exports = (app, modelsService) => {
 
+    const registerGetNotifications = () => {
+        const url = '/api/notification';
+        app.get(url,
+            (req, res) => {
+                service.getNotifications(modelsService)
+                    .then(result => res.status(200).send(result))
+                    .catch(err => res.status(500).send(err.message));
+            });
+        app.routesInfo['Notification'].push({ model: 'Notification', name: 'GetAll', method: 'GET', url: url });
+    }
+
     const registerSend = () => {
         const url = '/api/notification/:id/send';
         app.get(url,
@@ -13,6 +24,7 @@ module.exports = (app, modelsService) => {
         app.routesInfo['Notification'].push({ model: 'Notification', name: 'Send', method: 'GET', url: url });
     }
 
+    registerGetNotifications();
     registerSend();
 
 };
--- app/services/notification.service.js
+++ app/services/notification.service.js
@@ -2,6 +2,13 @@ const smsService = require('./sms.service');
 
 const service = {}
 
+service.getNotifications = async (modelsService) => {
+    const notifications = await modelsService.getModel('Notification').find({}).sort({ order: 1 });
+    const guests = await modelsService.getModel('Guest').find({});
+    const phoneGuests = guests.filter(g => g.phone && g.phone.startsWith('+'));
+    return notifications.map(n => Object.assign(n, { guests: countGuestsForNotification(n, phoneGuests) }));
+}
+
 service.send = async (modelsService, notificationId) => {
     const notification = await modelsService.getModel('Notification').findOne({ _id: notificationId });
     if (notification.type === 'Movil') {
@@ -9,6 +16,20 @@ service.send = async (modelsService, notificationId) => {
     }
     notification.sentTime = Date.now();
     return notification.save();
+}
+
+
+const countGuestsForNotification = (notification, guests) => {
+    let filteredGuests = [
+        ...guests
+    ];
+    if (notification.isBusNotification) {
+        filteredGuests = filteredGuests.filter(g => g.isTakingBus);
+        if (notification.stayingPlace) {
+            filteredGuests = filteredGuests.filter(g => g.stayingPlace === notification.stayingPlace);
+        }
+    }
+    return filteredGuests.length;
 }
 
 module.exports = service;
