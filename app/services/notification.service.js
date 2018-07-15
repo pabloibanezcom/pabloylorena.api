7fc59d7f24ac0201a28ff8e2dd3cd91269bc9b23
Pablo Ibanez <me@pabloibanez.com>
2018-07-15 11:48:02 +0100

    Notification changes

--- app/services/notification.service.js
+++ app/services/notification.service.js
@@ -5,7 +5,7 @@ const service = {}
 service.send = async (modelsService, notificationId) => {
     const notification = await modelsService.getModel('Notification').findOne({ _id: notificationId });
     if (notification.type === 'Movil') {
-        await smsService.sendSms(modelsService, notification.text);
+        await smsService.sendSms(modelsService, notification);
     }
     notification.sentTime = Date.now();
     return notification.save();
--- app/services/sms.service.js
+++ app/services/sms.service.js
@@ -1,33 +1,49 @@
 const Nexmo = require('nexmo')
 
 const nexmo = new Nexmo({
-    apiKey: process.env.NEXMO_API_KEY,
-    apiSecret: process.env.NEXMO_API_SECRET
+  apiKey: process.env.NEXMO_API_KEY,
+  apiSecret: process.env.NEXMO_API_SECRET
 })
 
 const from = process.env.SMS_FROM;
 
 const service = {};
 
-service.sendSms = async (modelsService, text) => {
-    const guestToSend = await modelsService.getModel('Guest').find({ sendSms: true }).populate({ path: 'table', select: 'number name' });
-    for (let guest of guestToSend) {
-        await sendSmsToPerson(guest, text);
-    }
-    return true;
+service.sendSms = async (modelsService, notification) => {
+  const guestToSend = await getGuestsToSend(modelsService, notification);
+  for (let guest of guestToSend) {
+    await sendSmsToPerson(guest, notification.text);
+  }
+  return true;
 }
 
 const sendSmsToPerson = async (person, text) => {
-    const transformedtext = text.replace('{{guest.name}}', person.name).replace('{{guest.table}}', `${person.table.number} - ${person.table.name}`);
-    if (process.env.ENABLE_SMS) {
-        await nexmo.message.sendSms(from, person.phone, transformedtext);
-    }
-    logSmsSent(from, person.phone, transformedtext);
-    return;
+  const transformedtext = text.replace('{{guest.name}}', person.name).replace('{{guest.table}}', `${person.table.number} - ${person.table.name}`);
+  if (process.env.ENABLE_SMS) {
+    await nexmo.message.sendSms(from, person.phone, transformedtext);
+  }
+  logSmsSent(from, person.phone, transformedtext);
+  return;
 }
 
 const logSmsSent = (from, number, text) => {
-    console.log(`SMS sent from ${from} to ${number} with text - ${text}`);
+  console.log(`SMS sent from ${from} to ${number} with text - ${text}`);
+}
+
+const getGuestsToSend = async (modelsService, notification) => {
+  const findQuery = buildFindQuery(notification);
+  return await modelsService.getModel('Guest').find(findQuery).populate({ path: 'table', select: 'number name' });
+}
+
+const buildFindQuery = (notification) => {
+  const query = { sendSms: true };
+  if (notification.isBusNotification) {
+    query.isTakingBus = true;
+    if (notification.stayingPlace) {
+      query.stayingPlace = notification.stayingPlace;
+    }
+  }
+  return query;
 }
 
 module.exports = service;
